63d2024e9270fe34792e2819386cbd9849a66a40
qwe7002 <qwe7002@hotmail.com>
2019-07-03 17:47:41 +0800

    解决unused

--- app/src/main/java/com/qwe7002/telegram_sms_compat/message_json.java
+++ app/src/main/java/com/qwe7002/telegram_sms_compat/message_json.java
@@ -1,6 +1,7 @@
 package com.qwe7002.telegram_sms_compat;
 
 class message_json {
+    @SuppressWarnings("unused")
     final boolean disable_web_page_preview = true;
     String parse_mode;
     String chat_id;

b9e5794a3e407073597bb858524dc48339d48108
qwe7002 <qwe7002@hotmail.com>
2019-08-19 18:41:06 +0800

    修改looper问题

--- app/src/main/java/com/qwe7002/telegram_sms_compat/main_activity.java
+++ app/src/main/java/com/qwe7002/telegram_sms_compat/main_activity.java
@@ -145,9 +145,13 @@ public class main_activity extends AppCompatActivity {
             call.enqueue(new Callback() {
                 @Override
                 public void onFailure(Call call, IOException e) {
+                    Looper.prepare();
                     progress_dialog.cancel();
+                    Looper.loop();
                     String error_message = error_head + e.getMessage();
+                    Looper.prepare();
                     Snackbar.make(v, error_message, Snackbar.LENGTH_LONG).show();
+                    Looper.loop();
                     public_func.write_log(context, error_message);
                 }
 
@@ -155,13 +159,16 @@ public class main_activity extends AppCompatActivity {
                 public void onResponse(Call call, Response response) throws IOException {
                     Looper.prepare();
                     progress_dialog.cancel();
+                    Looper.loop();
                     if (response.code() != 200) {
                         assert response.body() != null;
                         String result = response.body().string();
                         JsonObject result_obj = new JsonParser().parse(result).getAsJsonObject();
                         String error_message = error_head + result_obj.get("description").getAsString();
                         public_func.write_log(context, error_message);
+                        Looper.prepare();
                         Snackbar.make(v, error_message, Snackbar.LENGTH_LONG).show();
+                        Looper.loop();
                         return;
                     }
                     assert response.body() != null;
@@ -169,7 +176,9 @@ public class main_activity extends AppCompatActivity {
                     JsonObject result_obj = new JsonParser().parse(result).getAsJsonObject();
                     JsonArray chat_list = result_obj.getAsJsonArray("result");
                     if (chat_list.size() == 0) {
+                        Looper.prepare();
                         Snackbar.make(v, R.string.unable_get_recent, Snackbar.LENGTH_LONG).show();
+                        Looper.loop();
                         return;
                     }
                     final ArrayList<String> chat_name_list = new ArrayList<>();
@@ -209,7 +218,7 @@ public class main_activity extends AppCompatActivity {
                         }
                     }
                     main_activity.this.runOnUiThread(() -> new AlertDialog.Builder(v.getContext()).setTitle(R.string.select_chat).setItems(chat_name_list.toArray(new String[0]), (dialogInterface, i) -> chat_id.setText(chat_id_list.get(i))).setPositiveButton("Cancel", null).show());
-                    Looper.loop();
+
                 }
             });
         });
@@ -246,8 +255,10 @@ public class main_activity extends AppCompatActivity {
                 public void onFailure(@NonNull Call call, @NonNull IOException e) {
                     Looper.prepare();
                     progress_dialog.cancel();
+                    Looper.loop();
                     String error_message = error_head + e.getMessage();
                     public_func.write_log(context, error_message);
+                    Looper.prepare();
                     Snackbar.make(v, error_message, Snackbar.LENGTH_LONG)
                             .show();
                     Looper.loop();
@@ -255,16 +266,20 @@ public class main_activity extends AppCompatActivity {
 
                 @Override
                 public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                    Looper.prepare();
+
                     String new_bot_token = bot_token.getText().toString().trim();
+                    Looper.prepare();
                     progress_dialog.cancel();
+                    Looper.loop();
                     if (response.code() != 200) {
                         assert response.body() != null;
                         String result = response.body().string();
                         JsonObject result_obj = new JsonParser().parse(result).getAsJsonObject();
                         String error_message = error_head + result_obj.get("description");
                         public_func.write_log(context, error_message);
+                        Looper.prepare();
                         Snackbar.make(v, error_message, Snackbar.LENGTH_LONG).show();
+                        Looper.loop();
                         return;
                     }
                     if (!new_bot_token.equals(bot_token_save)) {
@@ -283,6 +298,7 @@ public class main_activity extends AppCompatActivity {
                     editor.apply();
                     public_func.stop_all_service(context);
                     public_func.start_service(context, battery_monitoring_switch.isChecked(), chat_command.isChecked());
+                    Looper.prepare();
                     Snackbar.make(v, R.string.success, Snackbar.LENGTH_LONG)
                             .show();
                     Looper.loop();

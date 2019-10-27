73a72914ec6397a3910d2c2b41307169475defaf
qwe7002 <qwe7002@hotmail.com>
2019-10-28 02:01:22 +0800

    实现扫码复制

--- app/src/main/java/com/qwe7002/telegram_sms_compat/main_activity.java
+++ app/src/main/java/com/qwe7002/telegram_sms_compat/main_activity.java
@@ -385,5 +385,15 @@ public class main_activity extends AppCompatActivity {
         }
         return true;
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                ((EditText) findViewById(R.id.bot_token)).setText(data.getStringExtra("bot_token"));
+            }
+        }
+    }
 }
 

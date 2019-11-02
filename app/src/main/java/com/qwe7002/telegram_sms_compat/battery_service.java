1cc7605306dc573c328e1dce3598993219ae3519
qwe7002 <qwe7002@hotmail.com>
2019-11-02 22:58:14 +0800

    修改receiver判断方法

--- app/src/main/java/com/qwe7002/telegram_sms_compat/battery_service.java
+++ app/src/main/java/com/qwe7002/telegram_sms_compat/battery_service.java
@@ -31,7 +31,6 @@ public class battery_service extends Service {
     static boolean doh_switch;
     private Context context;
     private battery_receiver battery_receiver = null;
-    private stop_broadcast_receiver stop_broadcast_receiver = null;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
@@ -49,25 +48,21 @@ public class battery_service extends Service {
         bot_token = sharedPreferences.getString("bot_token", "");
         doh_switch = sharedPreferences.getBoolean("doh_switch", true);
         final boolean charger_status = sharedPreferences.getBoolean("charger_status", false);
-        IntentFilter intentFilter = new IntentFilter(public_func.broadcast_stop_service);
-        stop_broadcast_receiver = new stop_broadcast_receiver();
-
         battery_receiver = new battery_receiver();
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_OKAY);
         filter.addAction(Intent.ACTION_BATTERY_LOW);
+        filter.addAction(public_func.broadcast_stop_service);
         if (charger_status) {
             filter.addAction(Intent.ACTION_POWER_CONNECTED);
             filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
         }
         registerReceiver(battery_receiver, filter);
-        registerReceiver(stop_broadcast_receiver, intentFilter);
 
     }
 
     @Override
     public void onDestroy() {
-        unregisterReceiver(stop_broadcast_receiver);
         unregisterReceiver(battery_receiver);
         stopForeground(true);
         super.onDestroy();
@@ -78,77 +73,74 @@ public class battery_service extends Service {
         return null;
     }
 
-    class stop_broadcast_receiver extends BroadcastReceiver {
+    class battery_receiver extends BroadcastReceiver {
         @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.i("battery_service", "Received stop signal, quitting now...");
-            stopSelf();
-            android.os.Process.killProcess(android.os.Process.myPid());
-        }
-    }
-
-}
-
-class battery_receiver extends BroadcastReceiver {
-    @Override
-    public void onReceive(final Context context, final Intent intent) {
-        Log.d("battery_receiver", "Receive action: " + intent.getAction());
-        String request_uri = public_func.get_url(battery_service.bot_token, "sendMessage");
-        final message_json request_body = new message_json();
-        request_body.chat_id = battery_service.chat_id;
-        StringBuilder message_body = new StringBuilder(context.getString(R.string.system_message_head) + "\n");
-        final String action = intent.getAction();
-        switch (Objects.requireNonNull(action)) {
-            case Intent.ACTION_BATTERY_OKAY:
-                message_body.append(context.getString(R.string.low_battery_status_end));
-                break;
-            case Intent.ACTION_BATTERY_LOW:
-                message_body.append(context.getString(R.string.battery_low));
-                break;
-            case Intent.ACTION_POWER_CONNECTED:
-                message_body.append(context.getString(R.string.charger_connect));
-                break;
-            case Intent.ACTION_POWER_DISCONNECTED:
-                message_body.append(context.getString(R.string.charger_disconnect));
-                break;
-        }
-        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = context.registerReceiver(null, filter);
-        assert batteryStatus != null;
-        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-        int battery_level = (int) ((level / (float) scale) * 100);
-        if (battery_level > 100) {
-            battery_level = 100;
-        }
-        request_body.text = message_body.append("\n").append(context.getString(R.string.current_battery_level)).append(battery_level).append("%").toString();
-        OkHttpClient okhttp_client = public_func.get_okhttp_obj(battery_service.doh_switch);
-        String request_body_raw = new Gson().toJson(request_body);
-        RequestBody body = RequestBody.create(public_func.JSON, request_body_raw);
-        Request request = new Request.Builder().url(request_uri).method("POST", body).build();
-        Call call = okhttp_client.newCall(request);
-        final String error_head = "Send battery info failed:";
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                e.printStackTrace();
-                String error_message = error_head + e.getMessage();
-                public_func.write_log(context, error_message);
-                if (action.equals(Intent.ACTION_BATTERY_LOW)) {
-                    public_func.send_fallback_sms(context, request_body.text);
-                }
+        public void onReceive(final Context context, final Intent intent) {
+            Log.d("battery_receiver", "Receive action: " + intent.getAction());
+            assert intent.getAction() != null;
+            if (intent.getAction().equals(public_func.broadcast_stop_service)) {
+                Log.i("battery_service", "Received stop signal, quitting now...");
+                stopSelf();
+                android.os.Process.killProcess(android.os.Process.myPid());
             }
-
-            @Override
-            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                if (response.code() != 200) {
-                    assert response.body() != null;
-                    String error_message = error_head + response.code() + " " + response.body().string();
+            String request_uri = public_func.get_url(battery_service.bot_token, "sendMessage");
+            final message_json request_body = new message_json();
+            request_body.chat_id = battery_service.chat_id;
+            StringBuilder message_body = new StringBuilder(context.getString(R.string.system_message_head) + "\n");
+            final String action = intent.getAction();
+            switch (Objects.requireNonNull(action)) {
+                case Intent.ACTION_BATTERY_OKAY:
+                    message_body.append(context.getString(R.string.low_battery_status_end));
+                    break;
+                case Intent.ACTION_BATTERY_LOW:
+                    message_body.append(context.getString(R.string.battery_low));
+                    break;
+                case Intent.ACTION_POWER_CONNECTED:
+                    message_body.append(context.getString(R.string.charger_connect));
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    message_body.append(context.getString(R.string.charger_disconnect));
+                    break;
+            }
+            IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            Intent batteryStatus = context.registerReceiver(null, filter);
+            assert batteryStatus != null;
+            int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+            int battery_level = (int) ((level / (float) scale) * 100);
+            if (battery_level > 100) {
+                battery_level = 100;
+            }
+            request_body.text = message_body.append("\n").append(context.getString(R.string.current_battery_level)).append(battery_level).append("%").toString();
+            OkHttpClient okhttp_client = public_func.get_okhttp_obj(battery_service.doh_switch);
+            String request_body_raw = new Gson().toJson(request_body);
+            RequestBody body = RequestBody.create(public_func.JSON, request_body_raw);
+            Request request = new Request.Builder().url(request_uri).method("POST", body).build();
+            Call call = okhttp_client.newCall(request);
+            final String error_head = "Send battery info failed:";
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    e.printStackTrace();
+                    String error_message = error_head + e.getMessage();
                     public_func.write_log(context, error_message);
+                    if (action.equals(Intent.ACTION_BATTERY_LOW)) {
+                        public_func.send_fallback_sms(context, request_body.text);
+                    }
                 }
-            }
-        });
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    if (response.code() != 200) {
+                        assert response.body() != null;
+                        String error_message = error_head + response.code() + " " + response.body().string();
+                        public_func.write_log(context, error_message);
+                    }
+                }
+            });
 
 
+        }
     }
+
 }
--- app/src/main/java/com/qwe7002/telegram_sms_compat/call_receiver.java
+++ app/src/main/java/com/qwe7002/telegram_sms_compat/call_receiver.java
@@ -42,71 +42,73 @@ public class call_receiver extends BroadcastReceiver {
         assert telephony != null;
         telephony.listen(custom_phone_listener, PhoneStateListener.LISTEN_CALL_STATE);
     }
-}
 
-class call_status_listener extends PhoneStateListener {
-    private static int lastState = TelephonyManager.CALL_STATE_IDLE;
-    private static String incoming_number;
-    private final Context context;
+    static class call_status_listener extends PhoneStateListener {
+        private static int lastState = TelephonyManager.CALL_STATE_IDLE;
+        private static String incoming_number;
+        private final Context context;
 
-    call_status_listener(Context context, String incoming_number) {
-        super();
-        this.context = context;
-        call_status_listener.incoming_number = incoming_number;
-    }
-
-    public void onCallStateChanged(int state, String incomingNumber) {
-        if (lastState == TelephonyManager.CALL_STATE_RINGING
-                && state == TelephonyManager.CALL_STATE_IDLE) {
-            final SharedPreferences sharedPreferences = context.getSharedPreferences("data", MODE_PRIVATE);
-            if (!sharedPreferences.getBoolean("initialized", false)) {
-                Log.i("call_status_listener", "Uninitialized, Phone receiver is deactivated.");
-                return;
-            }
-            String bot_token = sharedPreferences.getString("bot_token", "");
-            String chat_id = sharedPreferences.getString("chat_id", "");
-            String request_uri = public_func.get_url(bot_token, "sendMessage");
-            final message_json request_body = new message_json();
-            request_body.chat_id = chat_id;
-            request_body.text = "[" + context.getString(R.string.missed_call_head) + "]" + "\n" + context.getString(R.string.Incoming_number) + incoming_number;
-            String request_body_raw = new Gson().toJson(request_body);
-            RequestBody body = RequestBody.create(public_func.JSON, request_body_raw);
-            OkHttpClient okhttp_client = public_func.get_okhttp_obj(sharedPreferences.getBoolean("doh_switch", true));
-            Request request = new Request.Builder().url(request_uri).method("POST", body).build();
-            Call call = okhttp_client.newCall(request);
-            final String error_head = "Send missed call failed:";
-            call.enqueue(new Callback() {
-                @Override
-                public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                    e.printStackTrace();
-                    String error_message = error_head + e.getMessage();
-                    public_func.write_log(context, error_message);
-                    public_func.send_fallback_sms(context, request_body.text);
-                }
-
-                @Override
-                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                    assert response.body() != null;
-                    if (response.code() != 200) {
-
-                        String error_message = error_head + response.code() + " " + response.body().string();
-                        public_func.write_log(context, error_message);
-                        public_func.send_fallback_sms(context, request_body.text);
-                    } else {
-                        String result = response.body().string();
-                        JsonObject result_obj = JsonParser.parseString(result).getAsJsonObject().get("result").getAsJsonObject();
-                        String message_id = result_obj.get("message_id").getAsString();
-                        if (!public_func.is_phone_number(incoming_number)) {
-                            public_func.write_log(context, "[" + incoming_number + "] Not a regular phone number.");
-                            return;
-                        }
-                        public_func.add_message_list(message_id, incoming_number);
-                    }
-                }
-            });
+        call_status_listener(Context context, String incoming_number) {
+            super();
+            this.context = context;
+            call_status_listener.incoming_number = incoming_number;
         }
 
-        lastState = state;
+        public void onCallStateChanged(int state, String incomingNumber) {
+            if (lastState == TelephonyManager.CALL_STATE_RINGING
+                    && state == TelephonyManager.CALL_STATE_IDLE) {
+                final SharedPreferences sharedPreferences = context.getSharedPreferences("data", MODE_PRIVATE);
+                if (!sharedPreferences.getBoolean("initialized", false)) {
+                    Log.i("call_status_listener", "Uninitialized, Phone receiver is deactivated.");
+                    return;
+                }
+                String bot_token = sharedPreferences.getString("bot_token", "");
+                String chat_id = sharedPreferences.getString("chat_id", "");
+                String request_uri = public_func.get_url(bot_token, "sendMessage");
+                final message_json request_body = new message_json();
+                request_body.chat_id = chat_id;
+                request_body.text = "[" + context.getString(R.string.missed_call_head) + "]" + "\n" + context.getString(R.string.Incoming_number) + incoming_number;
+                String request_body_raw = new Gson().toJson(request_body);
+                RequestBody body = RequestBody.create(public_func.JSON, request_body_raw);
+                OkHttpClient okhttp_client = public_func.get_okhttp_obj(sharedPreferences.getBoolean("doh_switch", true));
+                Request request = new Request.Builder().url(request_uri).method("POST", body).build();
+                Call call = okhttp_client.newCall(request);
+                final String error_head = "Send missed call failed:";
+                call.enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                        e.printStackTrace();
+                        String error_message = error_head + e.getMessage();
+                        public_func.write_log(context, error_message);
+                        public_func.send_fallback_sms(context, request_body.text);
+                    }
+
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                        assert response.body() != null;
+                        if (response.code() != 200) {
+
+                            String error_message = error_head + response.code() + " " + response.body().string();
+                            public_func.write_log(context, error_message);
+                            public_func.send_fallback_sms(context, request_body.text);
+                        } else {
+                            String result = response.body().string();
+                            JsonObject result_obj = JsonParser.parseString(result).getAsJsonObject().get("result").getAsJsonObject();
+                            String message_id = result_obj.get("message_id").getAsString();
+                            if (!public_func.is_phone_number(incoming_number)) {
+                                public_func.write_log(context, "[" + incoming_number + "] Not a regular phone number.");
+                                return;
+                            }
+                            public_func.add_message_list(message_id, incoming_number);
+                        }
+                    }
+                });
+            }
+
+            lastState = state;
+        }
+
     }
 
 }
+

c46260ef83ff9e3345d08d8c8223d411240376a9
qwe7002 <qwe7002@hotmail.com>
2019-12-29 20:02:08 +0800

    简化代码

--- app/src/main/java/com/qwe7002/telegram_sms_compat/main_activity.java
+++ app/src/main/java/com/qwe7002/telegram_sms_compat/main_activity.java
@@ -96,38 +96,7 @@ public class main_activity extends AppCompatActivity {
         if (sharedPreferences.getBoolean("initialized", false)) {
             public_func.start_service(context, sharedPreferences.getBoolean("battery_monitoring_switch", false), sharedPreferences.getBoolean("chat_command", false));
             if (!sharedPreferences.getBoolean("conversion_data_structure", false)) {
-                new Thread(() -> {
-                    String message_list_raw = null;
-                    FileInputStream file_stream = null;
-                    try {
-                        file_stream = context.openFileInput("message.json");
-                        int length = file_stream.available();
-                        byte[] buffer = new byte[length];
-                        //noinspection ResultOfMethodCallIgnored
-                        file_stream.read(buffer);
-                        message_list_raw = new String(buffer);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } finally {
-                        if (file_stream != null) {
-                            try {
-                                file_stream.close();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                    if (message_list_raw != null) {
-                        JsonObject message_list = JsonParser.parseString(message_list_raw).getAsJsonObject();
-                        for (Map.Entry<String, JsonElement> entry_set : message_list.entrySet()) {
-                            Paper.book().write(entry_set.getKey(), entry_set.getValue());
-                            Log.d(TAG, "add_message_list: " + entry_set.getKey());
-                        }
-                        Log.d(TAG, "The conversion is complete.");
-                        public_func.write_file(context, "message.json", "", Context.MODE_PRIVATE);
-                    }
-                    sharedPreferences.edit().putBoolean("conversion_data_structure", true).apply();
-                }).start();
+                new Thread(() -> convert_data(sharedPreferences)).start();
             }
         }
 
@@ -472,6 +441,39 @@ public class main_activity extends AppCompatActivity {
                 ((EditText) findViewById(R.id.bot_token)).setText(data.getStringExtra("bot_token"));
             }
         }
+    }
+
+    private void convert_data(SharedPreferences sharedPreferences) {
+        String message_list_raw = null;
+        FileInputStream file_stream = null;
+        try {
+            file_stream = context.openFileInput("message.json");
+            int length = file_stream.available();
+            byte[] buffer = new byte[length];
+            //noinspection ResultOfMethodCallIgnored
+            file_stream.read(buffer);
+            message_list_raw = new String(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (file_stream != null) {
+                try {
+                    file_stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (message_list_raw != null) {
+            JsonObject message_list = JsonParser.parseString(message_list_raw).getAsJsonObject();
+            for (Map.Entry<String, JsonElement> entry_set : message_list.entrySet()) {
+                Paper.book().write(entry_set.getKey(), entry_set.getValue());
+                Log.d(TAG, "add_message_list: " + entry_set.getKey());
+            }
+            Log.d(TAG, "The conversion is complete.");
+            public_func.write_file(context, "message.json", "", Context.MODE_PRIVATE);
+        }
+        sharedPreferences.edit().putBoolean("conversion_data_structure", true).apply();
     }
 }
 
